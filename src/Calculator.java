e73c26f55c3b0af0446f97cd628e2fb22a322a28
k-ishiguro <k.ishiguro.jp@ieee.org>
2016-04-19 10:58:02 +0900

    Practicing JUnit and testing
    04/19, 2-16 Commit to share

---
+++ src/Calculator.java
@@ -0,0 +1,15 @@
+/**
+ * Written by Katsuhiko Ishiguro <ishiguro.katsuhiko@lab.ntt.co.jp>
+ * Last update: 04/16, 2016
+ */
+public class Calculator {
+
+    public int multiply(int x, int y){
+        return x * y;
+    }
+
+    public int divide(int x, int y){
+        return x / y;
+    }
+
+}
---
+++ test/CalculatorTest.java
@@ -0,0 +1,34 @@
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class CalculatorTest {
+
+    @Before
+    public void setUp() throws Exception {
+
+        //TODO: implement the setUp
+    }
+
+    @After
+    public void tearDown() throws Exception {
+
+        // implement the tearDown
+    }
+
+    @Test
+    public void testMultiply() throws Exception {
+
+        //TODO: implement the test
+        fail("not yet implemented");
+    }
+
+    @Test
+    public void testDivide() throws Exception {
+
+        //TODO: implement the test
+        fail("not yet implemented");
+    }
+}
